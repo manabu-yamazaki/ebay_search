b471b79d5125833d35140101e61da9ba627e9328
manabu-yamazaki <manabu.yamazaki.business@gmail.com>
2022-12-11 00:49:08 +0900

    completed

--- ebay_search.py
+++ ebay_search.py
@@ -13,7 +13,13 @@ from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.common.by import By
 
 from user_agent import USER_AGNETS
-from user_setting import CHROME_DRIVER_PATH, GOOGLE_CHROME_PATH, SEARCH_PAGE, SPREADSHEET_NAME, IS_HEADLESS
+from user_setting import (
+    CHROME_DRIVER_PATH,
+    GOOGLE_CHROME_PATH,
+    SEARCH_PAGE,
+    SPREADSHEET_NAME,
+    IS_HEADLESS,
+)
 
 FIRST_URL = "https://www.ebay.com/"
 SEARCH_URL_FORMAT = "{}&_ipg=240&_pgn={}"
@@ -98,25 +104,30 @@ def open_web_driver(user_agent, url):
 
 
 def init_config(driver):
-    element = driver.find_element(by=By.ID, value="gh-eb-Geo-a-default")
-    element.click()
-    time.sleep(1)
-    element = driver.find_element(by=By.ID, value="gh-eb-Geo-a-en")
-    element.click()
-    time.sleep(1)
-    element = driver.find_element(by=By.CSS_SELECTOR, value="#gh-shipto-click > div > button")
-    element.click()
-    time.sleep(1)
-    element = driver.find_element(by=By.CLASS_NAME, value="menu-button__button")
-    element.click()
-    element = driver.find_element(
-        by=By.CSS_SELECTOR, value='.menu-button__item:has(span > span[data-country="USA|US"])'
-    )
-    element.click()
-    time.sleep(1)
-    element = driver.find_element(by=By.CLASS_NAME, value="shipto__close-btn")
-    element.click()
-    time.sleep(3)
+    if driver.find_element(by=By.CLASS_NAME, value="gh-eb-Geo-txt").text != "English":
+        element = driver.find_element(by=By.ID, value="gh-eb-Geo-a-default")
+        element.click()
+        time.sleep(1)
+        element = driver.find_element(by=By.ID, value="gh-eb-Geo-a-en")
+        element.click()
+        time.sleep(2)
+    if (
+        len(driver.find_elements(by=By.CSS_SELECTOR, value='[aria-label="Ship to United States"]'))
+        == 0
+    ):
+        element = driver.find_element(by=By.CSS_SELECTOR, value="#gh-shipto-click > div > button")
+        element.click()
+        time.sleep(2)
+        element = driver.find_element(by=By.CLASS_NAME, value="menu-button__button")
+        element.click()
+        element = driver.find_element(
+            by=By.CSS_SELECTOR, value='.menu-button__item:has(span > span[data-country="USA|US"])'
+        )
+        element.click()
+        time.sleep(1)
+        element = driver.find_element(by=By.CLASS_NAME, value="shipto__close-btn")
+        element.click()
+        time.sleep(3)
 
 
 """
@@ -127,40 +138,22 @@ def init_config(driver):
 
 
 def init_google_tools():
-    # gauth = GoogleAuth()
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    # gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    #     "client_secret.json", scope
-    # )
     credentials = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
-    # client = gspread.authorize(gauth.credentials)
     client = gspread.authorize(credentials)
-    # return client.open(SPREADSHEET_NAME).sheet1, GoogleDrive(gauth)
-    now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9), "JST"))
-    return (
-        client.open(SPREADSHEET_NAME).worksheet("参照URL"),
-        client.open(SPREADSHEET_NAME).add_worksheet(
-            now.strftime("%Y%m%d%H%M%S"), rows=1000, cols=20
-        ),
+    now_str = datetime.datetime.now(
+        datetime.timezone(datetime.timedelta(hours=9), "JST")
+    ).strftime("%Y%m%d%H%M%S")
+    workbook = client.open(SPREADSHEET_NAME)
+    workbook.duplicate_sheet(
+        source_sheet_id=workbook.worksheet("テンプレート").id,
+        new_sheet_name=now_str,
+        insert_sheet_index=2,
     )
-
-
-"""
-概　要：情報収集するページの終端を取得する
-引　数：sheet:抽出結果シート
-返り値：検索結果のページ数、もしくは、限度ページ数
-"""
-
-
-def init_result_page(sheet):
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.SSN_COLUMN.value, "出品者")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.ITEM_URL_COLUMN.value, "商品ＵＲＬ")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.IMAGE_COLUMN.value, "画像")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.TITLE_COLUMN.value, "タイトル")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.PRICE_COLUMN.value, "金額")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.SEND_COLUMN.value, "送料")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.WATCHERS_COLUMN.value, "watchers数")
-    sheet.update_cell(SheetIndex.TITLE_ROW.value, SheetIndex.DATE_COLUMN.value, "出品日")
+    return (
+        workbook.worksheet("参照URL"),
+        workbook.worksheet(now_str),
+    )
 
 
 """
@@ -172,7 +165,7 @@ def init_result_page(sheet):
 
 def get_last_page(driver, url):
     driver.get(SEARCH_URL_FORMAT.format(url, 1))
-    time.sleep(1)
+    time.sleep(2)
     init_config(driver)
     if len(driver.find_elements(by=By.CLASS_NAME, value="srp-controls__count-heading")) == 0:
         return 0
@@ -198,59 +191,43 @@ def get_last_page(driver, url):
 def method_one_page(driver, sheet, row, ssn_name):
     print(ssn_name)
     for element in driver.find_elements(by=By.CLASS_NAME, value="s-item__wrapper"):
-        time.sleep(1)
         # "いいね"が付いてない商品はスキップ
         if len(element.find_elements(by=By.CLASS_NAME, value="s-item__watchCountTotal")) == 0:
             continue
 
+        # time.sleep(1)
+
         # 抽出結果ページに商品情報を追加
-        sheet.update_cell(row, SheetIndex.SSN_COLUMN.value, ssn_name)
-        sheet.update_cell(
-            row,
-            SheetIndex.ITEM_URL_COLUMN.value,
-            f'=HYPERLINK("{element.find_element(by=By.CLASS_NAME, value="s-item__link").get_attribute("href")}")',
+        cell_list = sheet.range(
+            row, SheetIndex.SSN_COLUMN.value, row, SheetIndex.DATE_COLUMN.value
         )
-        sheet.update_cell(
-            row,
-            SheetIndex.IMAGE_COLUMN.value,
-            f'=IMAGE("{element.find_element(by=By.CLASS_NAME, value="s-item__image-img").get_attribute("src")}")',
-        )
-        sheet.update_cell(
-            row,
-            SheetIndex.TITLE_COLUMN.value,
-            element.find_element(by=By.CSS_SELECTOR, value="a.s-item__link > div > span").text,
-        )
-        sheet.update_cell(
-            row,
-            SheetIndex.PRICE_COLUMN.value,
+        cell_list[SheetIndex.SSN_COLUMN.value - 1].value = ssn_name
+        cell_list[
+            SheetIndex.ITEM_URL_COLUMN.value - 1
+        ].value = f'=HYPERLINK("{element.find_element(by=By.CLASS_NAME, value="s-item__link").get_attribute("href")}")'
+        cell_list[
+            SheetIndex.IMAGE_COLUMN.value - 1
+        ].value = f'=IMAGE("{element.find_element(by=By.CLASS_NAME, value="s-item__image-img").get_attribute("src")}")'
+        cell_list[SheetIndex.TITLE_COLUMN.value - 1].value = element.find_element(
+            by=By.CSS_SELECTOR, value="a.s-item__link > div > span"
+        ).text
+        cell_list[SheetIndex.PRICE_COLUMN.value - 1].value = (
             element.find_element(by=By.CLASS_NAME, value="s-item__price")
             .text.replace("JPY ", "")
-            .replace("$", ""),
+            .replace("$", "")
         )
-        sheet.update_cell(
-            row,
-            SheetIndex.SEND_COLUMN.value,
-            element.find_element(by=By.CLASS_NAME, value="s-item__logisticsCost").text.replace(
-                "Free shipping", "0"
-            ),
-        )
-        sheet.update_cell(
-            row,
-            SheetIndex.WATCHERS_COLUMN.value,
-            element.find_element(
-                by=By.CSS_SELECTOR, value="span.s-item__watchCountTotal > span"
-            ).text.replace(" watchers", ""),
-        )
-        sheet.update_cell(
-            row,
-            SheetIndex.DATE_COLUMN.value,
-            datetime.datetime.strptime(
-                element.find_element(
-                    by=By.CSS_SELECTOR, value="span.s-item__listingDate > span"
-                ).text,
-                "%b-%d %H:%M",
-            ).strftime("%Y/%m/%d"),
-        )
+        cell_list[SheetIndex.SEND_COLUMN.value - 1].value = element.find_element(
+            by=By.CLASS_NAME, value="s-item__logisticsCost"
+        ).text.replace("Free shipping", "0")
+        cell_list[SheetIndex.WATCHERS_COLUMN.value - 1].value = element.find_element(
+            by=By.CSS_SELECTOR, value="span.s-item__watchCountTotal > span"
+        ).text.replace(" watchers", "")
+        cell_list[SheetIndex.DATE_COLUMN.value - 1].value = datetime.datetime.strptime(
+            element.find_element(by=By.CSS_SELECTOR, value="span.s-item__listingDate > span").text,
+            "%b-%d %H:%M",
+        ).strftime("%m/%d")
+
+        sheet.update_cells(cell_list, value_input_option="USER_ENTERED")
 
         row += 1
 
@@ -269,9 +246,6 @@ def start():
     url_sheet, result_sheet = init_google_tools()
     print("Google Tools Authorized")
 
-    # 抽出結果ページ初期処理
-    init_result_page(result_sheet)
-
     # 最初のページを開いて初期設定する
     driver = open_web_driver(USER_AGNETS, FIRST_URL)
     # element = driver.find_element(by=By.CSS_SELECTOR, value=".gh-ug-guest > a")
@@ -282,9 +256,6 @@ def start():
     # element.send_keys("god.of.icecream@gmail.com")
     # element = driver.find_element(by=By.ID, value="signin-continue-btn")
     # element.click()
-
-    # # 言語設定&配送先設定
-    # init_config(driver)
 
     # 参照URLリストを検索
     index = SheetIndex.START_ROW.value - 1
@@ -310,7 +281,7 @@ def start():
         ]
 
         # ページ数分繰り返し
-        for page in range(1, last_page):
+        for page in range(1, last_page + 1):
             driver.get(
                 SEARCH_URL_FORMAT.format(
                     url_sheet.cell(index, SheetIndex.URL_COLUMN.value).value, page
